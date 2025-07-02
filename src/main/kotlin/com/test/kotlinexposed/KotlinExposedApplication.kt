ebc2136e3081eb44ce38f252eb3c8d4b2f1cdf4e
khkang <khkang@shinsegae.com>
2025-07-02 17:32:00 +0900

    first commit

---
+++ src/main/kotlin/com/test/kotlinexposed/KotlinExposedApplication.kt
@@ -0,0 +1,11 @@
+package com.test.kotlinexposed
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinExposedApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinExposedApplication>(*args)
+}
---
+++ src/test/kotlin/com/test/kotlinexposed/KotlinExposedApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.test.kotlinexposed
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinExposedApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
