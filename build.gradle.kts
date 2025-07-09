8c751a97077a3262c0279ce8d530f4ac7d61d25d
khkang <khkang@shinsegae.com>
2025-07-09 14:38:33 +0900

    update readme

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,13 +19,12 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    // kotlin
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // spring boot
+    implementation("org.springframework.boot:spring-boot-starter-web")
 
     // jdbc
     runtimeOnly("org.postgresql:postgresql")
@@ -43,6 +42,9 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-dao:0.61.0")
     implementation("org.jetbrains.exposed:exposed-java-time:0.61.0")
     implementation("org.jetbrains.exposed:exposed-spring-boot-starter:0.61.0")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
